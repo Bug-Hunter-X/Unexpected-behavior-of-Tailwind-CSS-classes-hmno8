52f0e448aec914bd4b90868f26da76769ee3c140
BugHunter-X <askzenai@gmail.com>
2025-02-20 13:46:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// Import Tailwind directives
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+// Some code that uses Tailwind CSS classes
+div className="bg-gray-200 p-4 rounded shadow">
+  <p>This is some text.</p>
+</div>
+```
